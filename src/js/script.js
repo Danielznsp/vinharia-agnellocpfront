686bf90800ef94de0f0a933922132f2aa02ce219
Arthur Kazuo B K <arthur@ladio.com.br>
2026-04-06 10:38:25 -0300

    adicionando o aviso (alert)

--- src/js/script.js
+++ src/js/script.js
@@ -3,8 +3,7 @@ let nome = prompt("Digite o nome do vinho:");
 let tipo = prompt("Digite o tipo do vinho (Tinto, Branco, ou Rosé)");
 
 
-//Aviso para ver no console alert()
+//Aviso para ver no console alert()dsfds
 
+alert("Cadastro realizado! Veja os detalhes no console");
 
-
-//Informações coletadas console.log()
