69a502ad5d14f44437aa4bc254f3e2c23032b557
João Mello <joaofelipedepaulamello@gmail.com>
2026-04-06 10:12:58 -0300

    adicionei a variavel do tipo do vinho

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,6 @@
 //Coletando informações do usuário prompt()
 let nome = prompt("Digite o nome do vinho:");
+let tipo = prompt("Digite o tipo do vinho (Tinto, Branco, ou Rosé)");
 
 
 //Aviso para ver no console alert()
