cb4ee9607596d6d10e338d7e7c047a9f4d29e40c
Thiago <lutfithiago@gmail.com>
2026-04-06 22:27:25 -0300

    Ajustando o .js no index.html e adicionado alert no .js para melhor UX

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,17 @@
 //Coletando informações do usuário prompt()
+
 let nome = prompt("Digite o nome do vinho:");
+alert("Nome do vinho cadastrado!");
 let tipo = prompt("Digite o tipo do vinho (Tinto, Branco, ou Rosé): ");
+alert("Tipo do vinho cadastrado!");
+let quantidade = prompt("Digite a quantidade em estoque: ");
+alert("Quantidade em estoque do vinho cadastrado!");
 let safra = prompt("Digite a safra (ano): ");
-let quantidade = prompt("Digite a quantidade em estoque: ")
+alert("Safra do vinho cadastrada!");
 
 
 //Aviso para ver no console alert()
-alert("Cadastro realizado com sucesso,veja os detalhes no console. ")
+alert("Cadastro realizado com sucesso,veja os detalhes no console. ");
 
 
 //Informações coletadas console.log()
@@ -14,4 +19,5 @@ console.log("Cadastro do Vinho");
 console.log("-----------------------------");
 console.log("Nome:", nome);
 console.log("Tipo:", tipo);
+console.log("Safra(ano):", safra);
 console.log("Quantidade em estoque:" , quantidade);
